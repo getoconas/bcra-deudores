c5730ebc9a6aa7d39628cbdaf99445b3b8cb4319
getoconas <getoconas@gmail.com>
2026-03-15 13:38:40 -0300

    Add route to dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', 
+    component: DashboardComponent,
+    title: 'BCRA | Dashboard' // Angular 21 permite cambiar el título de la pestaña aquí mismo
+  },
+  {
+    path: '**', // Wildcard: si el usuario tipea una URL que no existe
+    redirectTo: '', // Lo redirigimos al inicio
+    pathMatch: 'full'
+  }
+];
